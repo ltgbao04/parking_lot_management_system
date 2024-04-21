dc020ac5447420f519c605a8bc3ebfee96e9ff62
ltgbao04 <giabaoletran96@gmail.com>
2024-04-21 22:33:49 +0700

    Just made some small changes

--- frontend/script.js
+++ frontend/script.js
@@ -15,3 +15,21 @@ tabs.forEach(tab => {
         target.classList.add('active')
     })
 })
+
+// Hàm để sinh số ngẫu nhiên từ 1 đến 200
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 200) + 1;
+}
+
+// Hàm để đặt số ngẫu nhiên vào ô textArea 'ticketID' mà vẫn giữ nguyên chữ 'MÃ PHIẾU'
+function setRandomNumberToTicketID() {
+    var randomNumber = generateRandomNumber();
+    var ticketIDTextArea = document.getElementById('ticketID');
+    ticketIDTextArea.value = "MÃ PHIẾU: " + randomNumber;
+}
+
+
+// Gọi hàm khi trang được tải
+window.onload = function() {
+    setRandomNumberToTicketID();
+};
